f87823adefdae2b8e8c6850c0552933d884bfbca
iamjerrie <jerrieiyanda@gmail.com>
2024-11-19 01:28:05 +0100

    First Python

---
+++ ass.py
@@ -0,0 +1,8 @@
+# Input student's age
+age = int(input("Enter the student's age: "))
+
+# Check if the student is eligible for admission
+if age <= 16:
+    print("Student admitted.")
+else:
+    print("Student not admitted.")
